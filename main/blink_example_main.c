2acff523b54ae716a37ad0686f74371a250cf389
rondindon <ronnie.rybarik@gmail.com>
2023-10-01 21:42:15 +0200

    nightrider

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -14,13 +14,12 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 
-#define BUTTON_PIN              18
-#define GREEN_PIN               21 
-#define ORANGE_PIN              22
-#define RED_PIN                 23
-#define PEDESTRIAN_GREEN_PIN    4
-#define PEDESTRIAN_RED_PIN      15
-#define TRAFFIC_LIGHT           2
+#define    FIRST            5 
+#define    SECOND           18
+#define    THIRD            19
+#define    FOURTH           21
+#define    FIFTH            22
+#define    SIXTH            23
 
 // variables will change:
 int led_state = 0;    // the current state of LED
@@ -33,67 +32,52 @@ static const char *TAG = "log :";
 
 void app_main(void)
 {
-    gpio_reset_pin(BUTTON_PIN);
-    gpio_reset_pin(GREEN_PIN);
-    gpio_reset_pin(ORANGE_PIN);
-    gpio_reset_pin(RED_PIN);
-    gpio_reset_pin(PEDESTRIAN_GREEN_PIN);
-    gpio_reset_pin(PEDESTRIAN_RED_PIN);
-    gpio_reset_pin(TRAFFIC_LIGHT);
-    gpio_set_direction(BUTTON_PIN, GPIO_PULLDOWN_ENABLE);
-    gpio_set_direction(GREEN_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(ORANGE_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(RED_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PEDESTRIAN_GREEN_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PEDESTRIAN_RED_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(TRAFFIC_LIGHT, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(FIRST);
+    gpio_reset_pin(SECOND);
+    gpio_reset_pin(THIRD);
+    gpio_reset_pin(FOURTH);
+    gpio_reset_pin(FIFTH);
+    gpio_reset_pin(SIXTH);
+    gpio_set_direction(FIRST, GPIO_MODE_OUTPUT);
+    gpio_set_direction(SECOND, GPIO_MODE_OUTPUT);
+    gpio_set_direction(THIRD, GPIO_MODE_OUTPUT);
+    gpio_set_direction(FOURTH, GPIO_MODE_OUTPUT);
+    gpio_set_direction(FIFTH, GPIO_MODE_OUTPUT);
+    gpio_set_direction(SIXTH, GPIO_MODE_OUTPUT);
 
     while (1) {
 
-
-        last_button_state = button_state;
-        button_state = gpio_get_level(BUTTON_PIN);
-        
-        if (last_button_state == 1 && button_state == 0) {
-              
-            ESP_LOGI(TAG, "The button is pressed");
-
-            // toggle state of LED
-            
-            led_state = 1;
-            gpio_set_direction(TRAFFIC_LIGHT, 1);
-
-            if(led_state == 1){
-
-                gpio_set_level(GREEN_PIN,1);
-                gpio_set_level(PEDESTRIAN_RED_PIN,1);
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-                gpio_set_level(GREEN_PIN,0);
-                gpio_set_level(PEDESTRIAN_RED_PIN,0);
-
-                gpio_set_level(ORANGE_PIN,1);
-                vTaskDelay(500 / portTICK_PERIOD_MS);
-                gpio_set_level(ORANGE_PIN,0);
-
-                gpio_set_level(RED_PIN,1);
-                gpio_set_level(PEDESTRIAN_GREEN_PIN,1);
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-                gpio_set_level(RED_PIN,0);
-                gpio_set_level(PEDESTRIAN_GREEN_PIN,0);
-
-                gpio_set_level(ORANGE_PIN,1);
-                vTaskDelay(500 / portTICK_PERIOD_MS);
-                gpio_set_level(ORANGE_PIN,0);
-
-                gpio_set_level(GREEN_PIN,1);
-                gpio_set_level(PEDESTRIAN_RED_PIN,1);
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-                gpio_set_level(GREEN_PIN,0);
-                gpio_set_level(PEDESTRIAN_RED_PIN,0);
-
-            }
+        gpio_set_level(FIRST,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(FIRST,0);
+        gpio_set_level(SECOND,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(SECOND,0);
+        gpio_set_level(THIRD,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(THIRD,0);
+        gpio_set_level(FOURTH,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(FOURTH,0);
+        gpio_set_level(FIFTH,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(FIFTH,0);    
+        gpio_set_level(SIXTH,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(SIXTH,0);
+        gpio_set_level(FIFTH,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(FIFTH,0);
+        gpio_set_level(FOURTH,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(FOURTH,0);
+        gpio_set_level(THIRD,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(THIRD,0);
+        gpio_set_level(SECOND,1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(SECOND,0);  
 
         }
 
     }
-}
